5da2376231aebbabdd28a3e7a7d088ba65ff8652
kiniak <kingakasicka@wp.pl>
2021-09-17 18:20:42 +0100

    I added tests

---
+++ Jenny's secret message/Jenny's secret message/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenny_s_secret_message
+{
+    public static class Kata
+    {
+        public static string greet(string name)
+        {
+            if (name == "Johnny")
+                return ("Hello, my love!");
+            else
+                return ("Hello, " + name + "!");
+
+        }
+    }
+}
---
+++ Jenny's secret message/Jenny's secret message/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jenny_s_secret_message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
