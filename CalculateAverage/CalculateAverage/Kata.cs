2e2d1d899661fa1f320eb93f94100b4efbe05c87
kiniak <kingakasicka@wp.pl>
2021-09-18 16:10:26 +0100

    Added notes

---
+++ CalculateAverage/CalculateAverage/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAverage
+{
+    public class Kata
+    {
+        public static double FindAverage(double[] array)
+        {
+            if (array.Length == 0)
+                return 0;
+
+            else
+                return array.Average();
+        }
+    }
+}
---
+++ CalculateAverage/CalculateAverage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculateAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
