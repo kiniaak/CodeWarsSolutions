f9dfaacfef9221d453d9b646b909a3d947f799fb
kiniak <kingakasicka@wp.pl>
2021-09-18 11:08:14 +0100

    Added ReadMe

---
+++ FakeBinary/FakeBinary/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeBinary
+{
+    class Kata
+    {
+        public static string FakeBin(string x)
+        {
+
+            char[] dig = x.ToCharArray();
+            List<char> columns = new List<char>(x.Length);
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (dig[i] < '5')
+                {
+                    columns.Add((char)('0'));
+                }
+                else if (dig[i] >= '5')
+                {
+                    columns.Add((char)('1'));
+                }
+            }
+            var myString = string.Join("", columns);
+            return myString;
+
+        }
+    }
+}
--- FakeBinary/FakeBinary/Program.cs
+++ FakeBinary/FakeBinary/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace FakeBinary
 {
     class Program
